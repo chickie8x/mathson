b3d4f895148f4981f8ad00f37c80d57fc2526451
unknown <chickie8x.qn@gmail.com>
2024-02-22 22:07:35 +0700

    fix

--- src/views/RankView/index.vue
+++ src/views/RankView/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="grid grid-cols-4 row-auto gap-12">
-    <div v-for="(item, idx) in matrix" :key="idx">
-      <MatrixComponent :matrix="item" :sum="retSum" />
-    </div>
+    
   </div>
 </template>
 
 <script>
 import { generateMatrix } from '@/data/index'
-import MatrixComponent from '@/components/kits/matrix/index.vue'
 
 export default {
   name: 'RankingView',
@@ -18,12 +15,8 @@ export default {
   },
 
   setup() {
-    const retSum = 8
-    const matrix = generateMatrix(retSum)
 
     return {
-      matrix,
-      retSum
     }
   }
 }
